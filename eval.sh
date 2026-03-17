b46d718297bfc1a1dab61da435107b6aa63454f1
heishuini <470789405@qq.com>
2026-03-17 23:07:33 +0800

    last commit

--- eval.sh
+++ eval.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=2 python eval.py \
-    --output_folder="/data/zfk/data/DIV2K/DIV2K_SR_VEPG_w12750/05_VEPG/valid" \
+CUDA_VISIBLE_DEVICES=1 python eval.py \
+    --output_folder="/data/zfk/data/DIV2K/DIV2K_SR_VEPG" \
     --target_folder="/data/zfk/data/DIV2K/DIV2K_valid_HR" \
     --metrics_save_path="/data/zfk/data/DIV2K/logs" \
--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
-# CUDA_VISIBLE_DEVICES=2 python test.py \
-#     --test_dir /data/zfk/data/DIV2K/DIV2K_test_LR_bicubic/X4 \
-#     --save_dir /data/zfk/data/DIV2K/DIV2K_SR_VEPG_w12750_wavelet \
-#     --model_id 5
 CUDA_VISIBLE_DEVICES=0 python test.py \
-    --valid_dir /data/zfk/data/DIV2K/DIV2K_valid_LR_bicubic/X4 \
-    --save_dir /data/zfk/data/DIV2K/DIV2K_SR_VEPG_w12750_wavelet \
+    --test_dir /data/zfk/data/DIV2K/DIV2K_test_LR_bicubic/X4 \
+    --save_dir /data/zfk/data/DIV2K/DIV2K_SR_VEPG \
     --model_id 5
+# CUDA_VISIBLE_DEVICES=0 python test.py \
+#     --test_dir /data/zfk/data/DIV2K/DIV2K_test_LR_bicubic/X4 \
+#     --valid_dir /data/zfk/data/DIV2K/DIV2K_valid_LR_bicubic/X4 \
+#     --save_dir /data/zfk/data/DIV2K/DIV2K_SR_VEPG \
+#     --model_id 5
